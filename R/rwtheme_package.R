3558f6573a23c75f48b684b995b4e92e372d2ffa
dennis feehan <dfeehan@princeton.edu>
2015-03-31 11:52:33 -0400

    first commit

---
+++ R/rwtheme_package.R
@@ -0,0 +1,13 @@
+##' ggplot2 theme for Rwanda survey
+##'
+##' This is a \pkg{ggplot2} theme that has been customized for the Rwanda
+##' 2011 survey. It is inspired by the \pkg{ggthemes} package, which has
+##' several nice themes collected together in package format.
+##'
+##' @docType package
+##' @name rwtheme
+##' @aliases rwtheme package-rwtheme
+##' @import ggplot2 
+NULL
+
+
